748c017ca8fddbfe64beac7abda63c4281e7a5ce
Yasser <103062506+Kleidin@users.noreply.github.com>
2023-10-02 10:08:35 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,36 @@
+-- TABLE
+CREATE TABLE cliente (
+  Nome varchar(100),
+  Endereço INT,
+  Email varchar(100),
+  Telefone INT
+);
+CREATE TABLE Fornecedor(
+	nome varcahr(100),
+  	endereço INT,
+  	email varchar(100),
+  	telefone INT
+);
+CREATE TABLE itemPedido(
+ idProduto varchar(100),
+  quantidade INT,
+   preço INT
+);
+CREATE TABLE Pedido (
+ dataPedido INT,
+  idCleinte INT,
+  produtoID
+);
+CREATE TABLE Produto(
+  nome varchar(100),
+  descrição varchar(100),
+  preço INT,
+  estoque varchar(100)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
